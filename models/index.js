541ed7e42dab320604dfe57af8b5cf9ee72ed981
Morrowii <sadler.m.ryan@gmail.com>
2018-08-25 13:25:22 -0400

    fixed database uri

--- models/index.js
+++ models/index.js
@@ -1,10 +1,10 @@
 
 const mongoose = require('mongoose');
-//const uri = process.env.MONGODB_URI;
+const uri = process.env.MONGODB_URI;
 
 module.exports = function() {
 
-    mongoose.connect('mongodb://localhost/herald_db');
+    mongoose.connect(uri);
 
     var db = mongoose.connection;
     db.on('error', console.error.bind(console, 'Connection error:'));
